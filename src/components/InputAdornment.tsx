0658c47defc91ae762473d39443571b373054f11
Bassem <bassem@coradine.com>
2021-03-24 22:17:40 +0000

    feat: Added NativeSelect flag dropdown variant when `native={true}`

--- src/components/InputAdornment.tsx
+++ src/components/InputAdornment.tsx
@@ -3,9 +3,10 @@ import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import Menu from "@material-ui/core/Menu";
 import MuiInputAdornment from "@material-ui/core/InputAdornment";
-// import NativeSelect from "@material-ui/core/NativeSelect";
+import NativeSelect from "@material-ui/core/NativeSelect";
 import { makeStyles } from "@material-ui/core/styles";
 import map from "lodash/map";
+import clsx from "clsx";
 
 import { DropdownItem } from "./Dropdown/Item";
 import { Country } from "../typings";
@@ -24,14 +25,12 @@ const useStyles = makeStyles(
 			padding: 8,
 		},
 		nativeRoot: {
-			padding: 0,
-
 			"& + svg": {
 				display: "none",
 			},
 		},
 		nativeSelect: {
-			padding: 0,
+			padding: "0!important",
 			lineHeight: 0,
 			height: 11,
 		},
@@ -43,30 +42,34 @@ const useStyles = makeStyles(
 );
 
 export type InputAdornmentProps = PhoneFieldStyles &
-	Pick<PhoneFieldProps, "enableSearch" | "localization"> & {
+	Pick<PhoneFieldProps, "enableSearch" | "localization" | "native"> & {
 		inputFlagClasses?: string;
 		flags: Record<string, HTMLElement | null>;
 		onlyCountries: Country[];
 		preferredCountries: Country[];
-		handleFlagItemClick: (country: Country, e: any) => void;
+		handleFlagItemClick: (country: Country | string) => void;
 	};
 
 export const InputAdornment = (props: InputAdornmentProps) => {
 	const {
 		dropdownClass,
 		flags = {},
+		native,
 		handleFlagItemClick,
 		inputFlagClasses,
 		localization,
-		// native = false,
 		onlyCountries = [],
 		preferredCountries = [],
 	} = props;
 	const classes = useStyles(props);
 	const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
 
-	const handleItemClick = (country: Country, event: React.MouseEvent) => {
-		handleFlagItemClick && handleFlagItemClick(country, event);
+	const handleItemClick = (country: Country | string) => {
+		handleFlagItemClick && handleFlagItemClick(country);
+		setAnchorEl(null);
+	};
+
+	const handleMenuClose = () => {
 		setAnchorEl(null);
 	};
 
@@ -80,41 +83,66 @@ export const InputAdornment = (props: InputAdornmentProps) => {
 		itemRef: (node: HTMLElement) => {
 			flags[`flag_no_${index}`] = node;
 		},
-		onClick: (event: React.MouseEvent) => handleItemClick(country, event),
+		onClick: () => handleItemClick(country),
+		native,
 	});
+
+	const commonDropdownProps = {
+		id: "country-menu",
+		onClose: handleMenuClose,
+	};
+
+	const dropdownContent = (
+		<>
+			{!!preferredCountries.length &&
+				map(preferredCountries, (country, index) => (
+					<DropdownItem {...commonDropdownItemProps(country, index)} />
+				))}
+			{!!preferredCountries.length && !native && <Divider />}
+
+			{map(onlyCountries, (country, index) => (
+				<DropdownItem {...commonDropdownItemProps(country, index)} />
+			))}
+		</>
+	);
 
 	return (
 		<MuiInputAdornment className={classes.positionStart} position="start">
-			<Button
-				disableRipple
-				className={classes.flagButton}
-				// aria-owns={anchorEl ? "country-menu" : null}
-				aria-label="Select country"
-				onClick={(e) => setAnchorEl(e.currentTarget)}
-				aria-haspopup
-			>
-				<div className={inputFlagClasses} />
-			</Button>
-			<Menu
-				className={dropdownClass}
-				id="country-menu"
-				anchorEl={anchorEl}
-				open={Boolean(anchorEl)}
-				onClose={() => setAnchorEl(null)}
-			>
-				{!!preferredCountries.length && (
-					<>
-						{map(preferredCountries, (country, index) => (
-							<DropdownItem {...commonDropdownItemProps(country, index)} />
-						))}
-						<Divider />
-					</>
-				)}
-
-				{map(onlyCountries, (country, index) => (
-					<DropdownItem {...commonDropdownItemProps(country, index)} />
-				))}
-			</Menu>
+			{native ? (
+				<NativeSelect
+					{...commonDropdownProps}
+					className={classes.native}
+					classes={{
+						root: clsx(classes.nativeRoot, inputFlagClasses),
+						select: classes.nativeSelect,
+					}}
+					onChange={(e) => handleItemClick(e.target.value)}
+					disableUnderline
+				>
+					{dropdownContent}
+				</NativeSelect>
+			) : (
+				<>
+					<Button
+						disableRipple
+						className={classes.flagButton}
+						// aria-owns={anchorEl ? "country-menu" : null}
+						aria-label="Select country"
+						onClick={(e) => setAnchorEl(e.currentTarget)}
+						aria-haspopup
+					>
+						<div className={inputFlagClasses} />
+					</Button>
+					<Menu
+						{...commonDropdownProps}
+						className={dropdownClass}
+						anchorEl={anchorEl}
+						open={Boolean(anchorEl)}
+					>
+						{dropdownContent}
+					</Menu>
+				</>
+			)}
 		</MuiInputAdornment>
 	);
 };
